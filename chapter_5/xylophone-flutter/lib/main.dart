726a1a7bc679e53c081a54296c5439aa4954b17d
Timo Knapp <mail@timoknapp.com>
2019-09-28 00:15:31 +0200

    [UPDATE] added functionality for chapter 5

--- chapter_5/xylophone-flutter/lib/main.dart
+++ chapter_5/xylophone-flutter/lib/main.dart
@@ -1,14 +1,46 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  void playSound(int soundNr) {
+    player.play('note$soundNr.wav');
+  }
+
+  Widget buildKey(int soundNr, Color color) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound(soundNr);
+        },
+        color: color,
+        child: Text(''),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
