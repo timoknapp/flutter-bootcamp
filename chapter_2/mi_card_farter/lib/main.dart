0d2789dbfd5ca9e534517016c80e91812a534ca3
Timo Knapp <mail@timoknapp.com>
2019-10-04 14:55:46 +0200

    [UPDATE] minor changes in main.dart

--- chapter_2/mi_card_farter/lib/main.dart
+++ chapter_2/mi_card_farter/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Mi Fart',
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
