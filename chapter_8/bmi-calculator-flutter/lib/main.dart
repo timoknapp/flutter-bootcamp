ba09b68749e2005ddc8904c97cfd01f5103a85b7
tea-mo903 <timo_knapp@hotmail.com>
2019-10-09 16:11:50 +0200

    [UPDATE] refactored files into multiple

--- chapter_8/bmi-calculator-flutter/lib/main.dart
+++ chapter_8/bmi-calculator-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -8,37 +9,9 @@ class BMICalculator extends StatelessWidget {
     return MaterialApp(
       home: InputPage(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
-        textTheme: TextTheme(
-          body1: TextStyle(
-            color: Color(0xFFFFFFFF),
-          ),
-        ),
-        accentColor: Colors.red,
-      ),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
       ),
     );
   }
