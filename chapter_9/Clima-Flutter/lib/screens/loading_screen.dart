19f73a08543500af84698aa49849216e31b4f947
tea-mo903 <timo_knapp@hotmail.com>
2019-10-11 22:45:50 +0200

    [UPDATE][chapter-9] started to play around with the geolocation plugin

--- chapter_9/Clima-Flutter/lib/screens/loading_screen.dart
+++ chapter_9/Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,6 +7,13 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Future<Position> getLocation() async {
+    Position currentPosition = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(currentPosition);
+    return currentPosition;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +21,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
