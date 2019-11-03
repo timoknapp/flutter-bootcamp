2fdb0f90fef69b809e0429fe1f823a749f7d873d
Timo Knapp <mail@timoknapp.com>
2019-11-03 22:06:25 +0100

    [UPDATE][chapter-11] remove debug banner

--- chapter_11/flash-chat-flutter/lib/main.dart
+++ chapter_11/flash-chat-flutter/lib/main.dart
@@ -10,6 +10,7 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData.dark().copyWith(
           textTheme: TextTheme(
             body1: TextStyle(color: Colors.black54),
