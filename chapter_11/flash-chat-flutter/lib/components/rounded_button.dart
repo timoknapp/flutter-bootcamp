9dcb9e063411be15ed34380b129309423ab5575f
Timo Knapp <mail@timoknapp.com>
2019-11-04 21:58:21 +0100

    [UPDATE][chapter-11] making roundedButton more beautiful

--- chapter_11/flash-chat-flutter/lib/components/rounded_button.dart
+++ chapter_11/flash-chat-flutter/lib/components/rounded_button.dart
@@ -22,6 +22,9 @@ class RoundedButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             title,
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
