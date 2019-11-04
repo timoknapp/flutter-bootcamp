1b9a7668d07374279ed60e9673feb8140793efc7
Timo Knapp <mail@timoknapp.com>
2019-11-04 21:11:12 +0100

    [UPDATE][chapter-11] added typewriter animation for apptitel

--- chapter_11/flash-chat-flutter/lib/screens/welcome_screen.dart
+++ chapter_11/flash-chat-flutter/lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -56,12 +57,13 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                     height: 60.0,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
                   ),
+                  isRepeatingAnimation: false,
                 ),
               ],
             ),
