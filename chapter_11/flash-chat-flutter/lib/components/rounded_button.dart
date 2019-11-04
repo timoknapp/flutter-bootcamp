a18fb9adb57d4a2657e51f459977d6e13b502db1
Timo Knapp <mail@timoknapp.com>
2019-11-04 21:30:31 +0100

    [UPDATE][chapter-11] added required property to rounded component

--- chapter_11/flash-chat-flutter/lib/components/rounded_button.dart
+++ chapter_11/flash-chat-flutter/lib/components/rounded_button.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class RoundedButton extends StatelessWidget {
-  RoundedButton({this.color, this.title, this.onPressed});
+  RoundedButton(
+      {@required this.color, @required this.title, @required this.onPressed});
 
   final Color color;
   final String title;
