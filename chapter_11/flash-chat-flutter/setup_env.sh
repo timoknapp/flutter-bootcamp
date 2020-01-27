24cb4d2af880367b1b425efffa09d9d4f2090d8e
tea-mo903 <timo_knapp@hotmail.com>
2020-01-27 22:56:53 +0100

    [UPDATE][chapter-11] more CICD fixes

---
+++ chapter_11/flash-chat-flutter/setup_env.sh
@@ -0,0 +1,8 @@
+echo "ENV:"
+echo $APP_DIR
+
+echo "Set iOS Env:"
+sed -i -e "s/GOOGLE_API_KEY_IOS/$GOOGLE_API_KEY_IOS/g" $APP_DIR/ios/Runner/GoogleService-Info.plist && sed -i -e "s/GOOGLE_APP_ID_IOS/$GOOGLE_APP_ID_IOS/g" $APP_DIR/ios/Runner/GoogleService-Info.plist && sed -i -e "s/GOOGLE_CLIENT_ID_IOS/$GOOGLE_CLIENT_ID_IOS/g" $APP_DIR/ios/Runner/GoogleService-Info.plist && sed -i -e "s/GOOGLE_CLIENT_ID_REV_IOS/$GOOGLE_CLIENT_ID_REV_IOS/g" $APP_DIR/ios/Runner/GoogleService-Info.plist && sed -i -e "s/GOOGLE_DATABASE_URL/$GOOGLE_DATABASE_URL/g" $APP_DIR/ios/Runner/GoogleService-Info.plist
+
+echo "Set Android Env:"
+sed -i -e "s/GOOGLE_API_KEY_ANDROID/$GOOGLE_API_KEY_ANDROID/g" $APP_DIR/android/app/google-services.json && sed -i -e "s/GOOGLE_APP_ID_ANDROID/$GOOGLE_APP_ID_ANDROID/g" $APP_DIR/android/app/google-services.json && sed -i -e "s/GOOGLE_CLIENT_ID_ANDROID/$GOOGLE_CLIENT_ID_ANDROID/g" $APP_DIR/android/app/google-services.json && sed -i -e "s/GOOGLE_DATABASE_URL/$GOOGLE_DATABASE_URL/g" $APP_DIR/android/app/google-services.json
