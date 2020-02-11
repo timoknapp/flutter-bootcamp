e0dd9a1b24f12b0ce8cb2f8c344998ef84d48924
tea-mo903 <timo_knapp@hotmail.com>
2020-02-11 19:51:22 +0100

    [UPDATE][chapter-11] add again encryption for secrets

---
+++ .github/scripts/decrypt_secret.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+--output /home/runner/work/flutter-bootcamp/flutter-bootcamp/chapter_11/flash-chat-flutter/android/app/google-services.json /home/runner/work/flutter-bootcamp/flutter-bootcamp/chapter_11/flash-chat-flutter/android/app/google-services.json.gpg
