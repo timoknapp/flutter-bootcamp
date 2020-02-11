c56a3e99b5eabfe87c54a55161e7cebc6eb0af3b
tea-mo903 <timo_knapp@hotmail.com>
2020-02-11 20:22:52 +0100

    [UPDATE][chapter-11] now cicd for android works too

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,3 +5,6 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output /home/runner/work/flutter-bootcamp/flutter-bootcamp/chapter_11/flash-chat-flutter/android/app/google-services.json /home/runner/work/flutter-bootcamp/flutter-bootcamp/chapter_11/flash-chat-flutter/android/app/google-services.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+--output /home/runner/work/flutter-bootcamp/flutter-bootcamp/chapter_11/flash-chat-flutter/ios/Runner/GoogleService-Info.plist /home/runner/work/flutter-bootcamp/flutter-bootcamp/chapter_11/flash-chat-flutter/ios/Runner/GoogleService-Info.plist.gpg
