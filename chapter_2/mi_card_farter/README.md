5348728320b56f581b926e6437f4c448514bda5b
tea-mo903 <timo_knapp@hotmail.com>
2020-02-20 21:49:20 +0100

    [UPDATE][chapter-2] re-enable apk build

--- chapter_2/mi_card_farter/README.md
+++ chapter_2/mi_card_farter/README.md
@@ -4,7 +4,7 @@
 
 ## Our Goal
 
-Now that you've seen how to create a Flutter app entirely from scratch, we're going to go further and learn more about how to design user interfaces for Flutter apps
+Now that you've seen how to create a Flutter app entirely from scratch, we're going to go further and learn more about how to design user interfaces for Flutter apps.
 
 ## What you will create
 
--- chapter_2/mi_card_farter/ios/Flutter/flutter_export_environment.sh
+++ chapter_2/mi_card_farter/ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/timo/development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/timo/workspace/flutter-bootcamp/chapter_2/mi_card_farter"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/timo/workspace/flutter-bootcamp/chapter_2/mi_card_farter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/timo/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
